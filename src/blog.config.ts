3d7fe52298cc702ab5ad95cbde69f1463dc034eb
rikuto06 <zhongweiluren0@gmail.com>
2026-02-17 15:27:46 +0900

    fix

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -2,7 +2,6 @@ import { z } from 'astro:content';
 
 export const blogSchema = z.object({
   title: z.string(),
-  description: z.string(),
   pubDate: z.coerce.date(),
   updatedDate: z.coerce.date().optional(),
   tags: z.array(z.string()).default([]),
